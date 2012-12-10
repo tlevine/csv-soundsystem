f19b4f052695628f65902cfeadb1a7537ed4bebe
Thomas Levine <occurrence@thomaslevine.com>
2012-12-10 03:55:04 -0500

    html build script

--- index.html.sh
+++ index.html.sh
@@ -1,14 +1,14 @@
-<html>
+#!/bin/sh
+
+echo '<html>
   <head>
     <style>
     * { margin: 0; padding: 0; width: 100%; height: 100%; }
     </style>
   </head>
   <body>
-    <pre>
-name,twitter,web
-aoeuaoeu,uaoeua,.uao
-aeua,.uaoeua,.
-    </pre>
+    <pre>'
+cat index.csv
+echo '    </pre>
   </body>
-</html>
+</html>'
